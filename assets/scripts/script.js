09385b9fc6b7a102fa6afd8943b0bbd8da1884ec
louisae452 <louisae452@msn.com>
2026-02-23 16:21:16 +0000

    Add link to script to game pages

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,10 @@
 // Wait for document to load befor running the script.
 document.addEventListener("DOMContentLoaded", function() {
+    //Event listener to show level choices when start button is clicked.
     let start = document.getElementById("start-button");
     let level_button = document.getElementById("choose-level");
     start.addEventListener("click", function() {
         level_button.classList.remove("hidden");
     });
+    //
 });
