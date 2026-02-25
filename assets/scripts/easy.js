89fcd7b4079efec928d412873e15dc361636c7dd
louisae452 <louisae452@msn.com>
2026-02-25 19:35:59 +0000

    Get cards to turn as they are clicked on - easy page

--- assets/scripts/easy.js
+++ assets/scripts/easy.js
@@ -2,14 +2,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     ////Event listenter to turn card back on click.
     let cardinner = document.getElementById("card-inner");
+    let turncard = document.querySelectorAll(".card-inner");
     
-    cardinner.addEventListener("click", function() {
-        
-        cardinner.style.transition = "transform 2s";
-        cardinner.style.transform = "rotateY(180deg)";
-        cardinner.style.transformStyle = "preserve-3d";
-        
+    
+    // Turn the cards as they are clicked on.
+    turncard.forEach(turncard => {
+        turncard.addEventListener ("click", function() {
+            this.style.transition = "transform 2s";
+            this.style.transform = "rotateY(180deg)";
+            this.style.transformStyle = "preserve-3d";
+
+        });
     });
+    
     
 
 });
