3df3ee11c47428b4493b270ec1fe1debfc39785f
louisae452 <louisae452@msn.com>
2026-02-26 16:33:31 +0000

    Turn only 2 cards at a time

--- assets/scripts/easy.js
+++ assets/scripts/easy.js
@@ -1,20 +1,32 @@
 // Wait for document to load befor running the script.
+let number_turns=0;
+let turncard = document.querySelectorAll(".card-inner");
 document.addEventListener("DOMContentLoaded", function() {
-    ////Event listenter to turn card back on click.
-    let cardinner = document.getElementById("card-inner");
-    let turncard = document.querySelectorAll(".card-inner");
-    
-    
-    // Turn the cards as they are clicked on.
-    turncard.forEach(turncard => {
-        turncard.addEventListener ("click", function() {
-            this.style.transition = "transform 2s";
-            this.style.transform = "rotateY(180deg)";
-            this.style.transformStyle = "preserve-3d";
 
-        });
-    });
+    ////Event listenter to turn card back on click.    
+   turncard.forEach(turncard => {     
+    turncard.addEventListener("click", turncard2);
+   });   
     
-    
-
+            
 });
+
+// Function to turn cards on click. (stops after 2 cards have been turned).
+function turncard2(e) {               
+    let card = e.currentTarget;                
+    card.style.transition = "transform 2s";
+    card.style.transform = "rotateY(180deg)";
+    card.style.transformStyle = "preserve-3d";
+    number_turns++;
+    console.log(number_turns); // DELETE LATER
+    // Remove event listener after 2 cards have been turned .            
+    if (number_turns == 2) {
+        turncard.forEach(turncard => { 
+            turncard.removeEventListener("click", turncard2);
+        });                     
+    }
+}    
+       
+
+
+    
